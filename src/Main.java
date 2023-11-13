bf769530eca87f656ec29a552f9c8df03e41ff6c
James <naurotj@gmail.com>
2023-11-13 09:31:42 -0500

    First commit. seems stable. need to clean up a bit/refactor

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+import static java.net.InetAddress.getLocalHost;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int port = 11122;
+        int sPort =0;
+        InetAddress sAddr = null;
+        byte[] receiveBuffer = new byte[1029];
+        byte[] resTmp = new byte[1029];
+
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Enter a url: ");
+        String url = cin.nextLine();
+
+        int packetLength = url.length() + 5;
+        byte[] tmp = new byte[packetLength];
+        byte[] length = ByteBuffer.allocate(4).putInt(packetLength).array();
+        byte seq = 0;
+        byte[] msg = url.getBytes();
+        tmp[0] = seq;
+        tmp[1] = length[0];
+        tmp[2] = length[1];
+        tmp[3] = length[2];
+        tmp[4] = length[3];
+        for (int i = 0; i < url.length(); i++)
+            tmp[i + 5] = msg[i];
+
+        DatagramPacket request = new DatagramPacket(tmp, packetLength, getLocalHost(), port);
+        DatagramPacket response = new DatagramPacket(receiveBuffer, 1029);
+        DatagramSocket datagramSocket = new DatagramSocket();
+
+        datagramSocket.send(request);
+        System.out.println("Sent message: " + url);
+        boolean initial = true;
+        int tmp1 = 0;
+        do {
+            System.out.println("Waiting for response");
+            datagramSocket.receive(response);
+            if (initial) {
+                initial = false;
+                sPort = response.getPort();
+                sAddr = response.getAddress();
+                tmp1 = 0;
+            }
+
+            resTmp = response.getData();
+            System.out.println("\tReceived packet. Seq#" + resTmp[0]);
+            System.out.println("\tExpected seq#" + tmp1);
+            if (tmp1 == resTmp[0]) {
+                length[0] = resTmp[1];
+                length[1] = resTmp[2];
+                length[2] = resTmp[3];
+                length[3] = resTmp[4];
+                packetLength = new BigInteger(length).intValue();
+                System.out.println("\tPacket length: " + packetLength);
+                byte[] packet = new byte[1024];
+                for (int i = 0; i < packetLength - 5; i++)
+                    packet[i] = resTmp[i + 5];
+                request = new DatagramPacket(getAck((byte) tmp1), 5, sAddr, sPort);
+                System.out.println("\tSending ack" + tmp1);
+                datagramSocket.send(request);
+                tmp1 = tmp1 ^ 1;
+            } else {
+                System.out.println("\t***** wrong seq#");
+                int tmp2 = tmp1 ^ 1;
+                System.out.println("\tSending ack" + tmp2);
+                datagramSocket.send(new DatagramPacket(getAck((byte) tmp2), 5, sAddr, sPort));
+            }
+        } while (packetLength > 1028);
+    }
+
+    public static byte[] getAck(byte ack) {
+        byte[] retValue = new byte[5];
+        retValue[0] = ack;
+        retValue[1] = 0;
+        retValue[1] = 0;
+        retValue[1] = 0;
+        retValue[1] = 0;
+        return retValue;
+
+    }
+}
